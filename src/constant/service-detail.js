fb99cf0464fa438c2d1b080a367039792309921e
arpan <shaharpan05@gmail.com>
2022-11-24 12:29:05 +0530

    add service detail

--- src/constant/service-detail.js
+++ src/constant/service-detail.js
@@ -35,22 +35,22 @@ export const services = [
     }
     ],
   },
-  {
-    title: "Domestic taxation",
-    key: "domestic-tax",
-    link: '/service/domestic-tax',
-    description: `statutory audit, tax audit, internal audit, concurrent
-  audit, management audit, compliance review audit, fraud investigation audit, GST audit, and
-  information system audit services`
-  },
-  {
-    title: "Indirect Tax",
-    key:"indirect-tax",
-    link: '/service/indirect-tax',
-    description: `statutory audit, tax audit, internal audit, concurrent
-  audit, management audit, compliance review audit, fraud investigation audit, GST audit, and
-  information system audit services`
-  },
+  // {
+  //   title: "Domestic taxation",
+  //   key: "domestic-tax",
+  //   link: '/service/domestic-tax',
+  //   description: `statutory audit, tax audit, internal audit, concurrent
+  // audit, management audit, compliance review audit, fraud investigation audit, GST audit, and
+  // information system audit services`
+  // },
+  // {
+  //   title: "Indirect Tax",
+  //   key: "indirect-tax",
+  //   link: '/service/indirect-tax',
+  //   description: `statutory audit, tax audit, internal audit, concurrent
+  // audit, management audit, compliance review audit, fraud investigation audit, GST audit, and
+  // information system audit services`
+  // },
   {
     title: "International Tax",
     description: `Global tax outflow is the key consideration for any business entities
@@ -64,14 +64,15 @@ export const services = [
       globalisation, business entities are frequently accessing multiple
       foreign jurisdictions for buying and selling of goods and services
       with motive of profit which triggers taxation in either jurisdiction.
-      India has entered treaty with many countries to avoid double
-      taxation arising as a result of any transaction.`,
+      One of key consideration for each country is to have its fair share
+      of tax triggering as a result of profit generated from cross border
+      transactions. In order to curb double taxation India has entered
+      treaty with many countries.`,
       imgPath: '/assets/img/valuation/valuation1.jpg'
-
     },
     {
       type: "list",
-      title: "Our deliverables include::",
+      title: "Our key deliverables in relation to international taxation wing are:",
       content: [
         [`Assessing tax impact on transactions by Indian residents with non-residents.`],
         [`Guiding on foreign tax credits/tax rebates/exemption`],
@@ -97,18 +98,24 @@ export const services = [
     key: 'foreign-exchange',
     content: [{
       type: "paragraph",
-      content: `RBI is keeping watch any transactions such as purchase of
-      movable or immovable assets, investment in shares or securities,
-      export or import transactions between residents and Non-
-      Resident. There are rule and regulations prescribed by RBI to report
-      such transactions and keep appropriate records which help to
-      monitor on forex reserves. Foreign national/NRI willing to set up
-      business in India has to comply with Foreign Direct Investment
-      (FDI) regulations and Indian residents willing to set up business
-      outside India has to comply with Overseas Direct Investment (ODI)
-      regulations.`,
+      content: `RBI is keeping watch on every transaction effecting foreign
+      exchange inflow/outflow such as purchase of movable or
+      immovable assets, inbound/outbound investment in shares or
+      securities, export or import transactions, remittance for personal
+      purpose etc. There are rule and regulations prescribed by RBI to
+      report such transactions and keep appropriate records which help
+      to monitor on forex reserves.`,
       imgPath: '/assets/img/valuation/valuation1.jpg'
-
+    },
+    {
+      type: "paragraph",
+      content: `Foreign national/NRI willing to set up business in India has to
+      comply with Foreign Direct Investment (FDI) regulations and Indian
+      residents willing to set up business outside India has to comply
+      with Overseas Direct Investment (ODI) regulations. SNSS assist
+      Resident Indian/Non-Resident Indian (NRI)/Foreign Companies on
+      following areas`,
+      imgPath: '/assets/img/valuation/valuation1.jpg'
     },
     {
       type: "list",
@@ -149,31 +156,270 @@ export const services = [
       {
         type: "paragraph",
         content: `There are three types of company set up options available to
-        businessman. First one is Mainland Company which is the more
-        preferable to trade anywhere in UAE and GCC. Second one is
-        Freezone Company which offer a vibrant, dynamic working
-        environment and encourage foreign investment. The efficient
-        business laws, easier labour and immigration procedures,
-        exemption from taxes, encourage entrepreneurs to rush to free
-        zone incorporation. Third one is an Offshore company is
-        incorporated in a low-tax or no-tax jurisdiction for the purpose of
-        legally minimizing the tax payment and gain confidentiality over
-        one’s financial affairs.`,
+        business man. One is main land company, second is Free-zone
+        Company and third one is an Offshore company. Our hassle-free
+        services and well-complied business structure improve comfort
+        level of Indian entrepreneurs.`,
         imgPath: '/assets/img/valuation/valuation1.jpg'
       },
       {
         type: "list",
         title: null,
         content: [
-          [`Company Set up in UAE`],
+          [`Assisting in selection of Location for Incorporation`],
+          [`Registration of Company with authorities`],
+          [`Assisting in Bank account opening and other ancillary services like visa, Emirates, medical, insurance etc.`],
+          [`Guiding on selection of business center for physical office`],
           [`Book Keeping and Financial planning`],
           [`Providing necessary platform to commence operations in UAE including support to bank accounts`],
-          [`UAE Vat Consultation`],
-          [`UAE Vat return Filing`],
+          [`Vat Consultation`],
+          [`Vat return Filing`],
           [`Economic Substance Regulation (ESR)`],
-          [`Country – by – Country reporting for UAE entitiesa`],
+          [`Country – by – Country reporting for UAE entities`],
           [`ERP Implementation`],
           [`Business Valuation of UAE entities`]
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "GIFT -IFSC",
+    description: `GIFT City is an integrated development on 886 acres of land with
+    62 mn sq. ft. of Built Up. GIFT City consist of a conducive Multi-Service SEZ (Special Economic Zone) and an exclusive Domestic
+    Are.`,
+    link: '/service/gift-ifsc',
+    key: 'gift-ifsc',
+    content: [
+      {
+        type: "paragraph",
+        content: `GIFT City is an integrated development on 886 acres of land with
+        62 mn sq. ft. of Built Up. GIFT City consist of a conducive Multi-
+        Service SEZ (Special Economic Zone) and an exclusive Domestic
+        Area. GIFT City is a new Financial &amp; Technology Gateway of India
+        for the World. It is the new business destination offering
+        competitive edge to financial services and Technology related
+        activities. The government of India is offering many tax sops and
+        relief from procedural compliances to attain object of ease of
+        doing business for IFSC units set up in GIFT. We assist ventures in
+        following areas.`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`Guiding on setting up of entities`],
+          [`Registration of entities`],
+          [`Application for Bond`],
+          [`NSDL Registration`],
+          [`Liaison with Officers for receipt of Capital Goods, its entry into SEZ and maintenance of record thereof`],
+          [`Filling of Online Entry regarding DTA procurement of GST goods`],
+          [`Re-warehousing`],
+          [`Preparing and Filling of Monthly Reports`],
+          [`Filling Service Export Reporting Form`],
+          [`Filling of Annual Report`],
+          [`Filling of Gist of Contract/Softex`],
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "Transfer Pricing Compliances",
+    description: `In the current global environment, companies have operations
+    across the globe. Any business transactions with associated
+    enterprise (i.e. foreign related party) triggers transfer pricing
+    regulation in India.`,
+    link: '/service/transfer-pricing-complience',
+    key: 'transfer-pricing-complience',
+    content: [
+      {
+        type: "paragraph",
+        content: `In the current global environment, companies have operations
+        across the globe. Any business transactions with associated
+        enterprise (i.e. foreign related party) triggers transfer pricing
+        regulation in India. India has also incorporated domestic transfer
+        pricing regulation for certain specified transactions. We ensure
+        that our clients adhere to the appropriate transfer
+        pricing regulations. Our advice and legal support in relation to
+        transfer pricing regulations covers following.`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`Preparation of Transfer Pricing Study report including benchmarking analysis`],
+          [`Advising on transfer pricing policies considering local laws as well as Base Erosion Practice Standards (BEPS) action plan of OCED.`],
+          [`Representation before transfer pricing authorities`],
+          [`Assisting in Country By Country Reporting of MNCs in India`],
+          [`Advising and consulting on Thin Capitalization Rule`],
+          [`Assisting in preparation and filing an application under Safe Harbor Rules`],
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "Start Up Advisory Services",
+    description: `An entrepreneur at the initial stage has to consider so many
+    unknown factors for success of any idea or business model.`,
+    link: '/service/start-up-advisory',
+    key: 'start-up-advisory',
+    content: [
+      {
+        type: "paragraph",
+        content: `An entrepreneur at the initial stage has to consider so many
+        unknow factors for success of any idea or business model. Many
+        new ventures face certain legal challenges at later stage when the
+        idea is already grown may be due to lack of entrepreneur’s
+        knowledge or inappropriate advice. Having experts on board can
+        significantly improve the chances of success as well as saving of
+        time and cost. When starting a new business, most entrepreneurs
+        do not have the expertise across all areas of the business. SNSS
+        helps new businesses as under`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`Guiding on legal structuring of entities`],
+          [`Guiding on Investment Instruments`],
+          [`Start Up registration`],
+          [`Start Up Valuation`],
+          [`Review/Drafting of shareholder’s agreement`],
+          [`Liaisoning between Investors and start up owner`],
+          [`Guiding on Foreign Direct Investment in Start Ups`],
+          [`Review of Start Up’s Business Plan`],
+          [`Study on Financial feasibility of Start up’s`],
+          [`Assisting in preparation of Pitch Documents`],
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "Out Sourced CFO",
+    description: `Outsourcing of functions related to CFO is very benefiting and
+    smart step for small and medium size service entities and start up
+    owners.`,
+    link: '/service/out-sourced-cfo',
+    key: 'out-sourced-cfo',
+    content: [
+      {
+        type: "paragraph",
+        content: `Outsourcing of functions related to CFO is very benefiting and
+        smart step for small and medium size service entities and start up
+        owners. It reduces the burden of the owners, management or
+        stake holders for legal and financial compliances. It is also termed
+        as Virtual CFO as the functions can be undertaken without
+        physically visiting office premise. The offerings under this tab
+        includes following`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`Book Keeping`],
+          [`Accounts payable and Accounts receivable management`],
+          [`Payroll processing`],
+          [`Month end compliances and reporting`],
+          [`Yearly compliances and reporting`],
+          [`Budgeting and cashflow management`],
+          [`Year closure support`],
+          [`KPI analysis`],
+          [`Cost accounting`],
+          [`Financing solutions`],
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "Audit and Assurance",
+    description: `Audit of books of accounts involves inspecting and testing the
+    correctness of transactions already undertaken.`,
+    link: '/service/audit-assurance',
+    key: 'audit-assurance',
+    content: [
+      {
+        type: "paragraph",
+        content: `Audit of books of accounts involves inspecting and testing the
+        correctness of transactions already undertaken. This process is
+        undertaken by businesses either compulsorily by virtue of statue
+        or voluntarily at the option of management.`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      }
+    ]
+  },
+  {
+    title: "Direct Tax Compliances",
+    description: `Every resident in India as well as Non-residents earning income
+    from Indian sources are touching the boundaries of income tax
+    law in India..`,
+    link: '/service/direct-tax',
+    key: 'direct-tax',
+    content: [
+      {
+        type: "paragraph",
+        content: `Every resident in India as well as Non-residents earning income
+        from Indian sources are touching the boundaries of income tax
+        law in India. Having earned income in India tax payer has to
+        comply with various direct tax provisions. Our services cover
+        following areas`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`Tax efficient business structuring`],
+          [`Preparation and filing of Tax Returns`],
+          [`Tax Planning`],
+          [`Handling litigation and Representation before the authorities`],
+          [`Appearing before the Appellate Authority`],
+          [`Consultancy for withholding of taxes`],
+          [`Advising on NRI taxation and filing of ITRs`],
+          [`Preparing and filing TDS Returns`],
+          [`Assisting organization to resolve the previous year TDS defaults`],
+        ],
+        imgPath: '/assets/img/valuation/valuation2.jpg'
+      }
+    ]
+  },
+  {
+    title: "GST advisory Services",
+    description: `With introduction of GST Laws in India, big business houses, small
+    and medium entities are looking for GST professionals for
+    implementation of new law.`,
+    link: '/service/gst',
+    key: 'gst',
+    content: [
+      {
+        type: "paragraph",
+        content: `With introduction of GST Laws in India, big business houses, small
+        and medium entities are looking for GST professionals for
+        implementation of new law. Our team of experts provides following
+        services assisting smooth functioning of business.`,
+        imgPath: '/assets/img/valuation/valuation1.jpg'
+      },
+      {
+        type: "list",
+        title: null,
+        content: [
+          [`GST Registration`],
+          [`GST Returns`],
+          [`GST Audit`],
+          [`Monthly consulting for routine GST compliances`],
+          [`GST Compliance Review`],
+          [`Refund of Input Tax Credit`],
+          [`Impact analysis of GST applicability to Industry`],
+          [`Obtaining Bond/Letter of Undertaking (LUT)`],
+          [`Representation before state authorities for concluding assessment under Value Added Tax`],
+          [`Representation before service tax authorities for concluding assessments/assisting service audit of the clients.`],
         ],
         imgPath: '/assets/img/valuation/valuation2.jpg'
       }
