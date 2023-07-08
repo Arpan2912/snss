d00bd0dda89061c8940bfed3c50672062abf93d7
Arpan <shaharpan05@gmail.com>
2023-07-08 10:09:37 +0530

    Add Startup Category

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -8,82 +8,87 @@ import { useNavigate, useLocation } from 'react-router'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHeader from '../components/PageHeader';
+import { categories, subCategories } from '../constant/category';
 
-const categories = [
-  {
-    name: 'Valuation Scoop',
-    value: 'valuation'
-  },
-  {
-    name: 'Tax Tidings',
-    value: 'tax'
-  },
-  {
-    name: 'FEMA Flash',
-    value: 'fema'
-  },
-  {
-    name: 'Global Business Setup',
-    value: 'global_business_setup'
-  }
-];
+// const categories = [
+//   {
+//     name: 'Valuation Scoop',
+//     value: 'valuation'
+//   },
+//   {
+//     name: 'Tax Tidings',
+//     value: 'tax'
+//   },
+//   {
+//     name: 'FEMA Flash',
+//     value: 'fema'
+//   },
+//   {
+//     name: 'Global Business Setup',
+//     value: 'global_business_setup'
+//   },
+//   {
+//     name: 'Startup Sparkle',
+//     value: 'startup'
+//   }
+// ];
 
-const subCategories = {
-  tax: [
-    {
-      name: 'Direct Tax',
-      value: 'direct_tax'
-    },
-    {
-      name: 'International Tax',
-      value: 'international_tax'
-    },
-    {
-      name: 'Transfer Pricing',
-      value: 'transfer_pricing'
-    },
-    {
-      name: 'GST',
-      value: 'gst'
-    },
-    {
-      name: 'NRI taxation',
-      value: 'nri_taxation'
-    }
-  ],
-  fema: [
-    {
-      name: 'Foreign Direct Investment',
-      value: 'foreign_direct_investment'
-    },
-    {
-      name: 'Overseas Investment',
-      value: 'overseas_investment'
-    },
-    {
-      name: 'External Commercial Borrowings',
-      value: 'external_commercial_borrowings'
-    },
-    {
-      name: 'Dealing In Immovable properties by NRI',
-      value: 'dealing_in_immovable_properties_by_nri'
-    }
-  ],
-  global_business_setup: [
-    {
-      name: 'United Arab Emirates(UAE)',
-      value: 'uae'
-    },
-    {
-      name: 'United States of America(USA)',
-      value: 'usa'
-    },
-    {
-      name: 'Canada',
-      value: 'canada'
-    }
-  ]
-}
+// const subCategories = {
+//   tax: [
+//     {
+//       name: 'Direct Tax',
+//       value: 'direct_tax'
+//     },
+//     {
+//       name: 'International Tax',
+//       value: 'international_tax'
+//     },
+//     {
+//       name: 'Transfer Pricing',
+//       value: 'transfer_pricing'
+//     },
+//     {
+//       name: 'GST',
+//       value: 'gst'
+//     },
+//     {
+//       name: 'NRI taxation',
+//       value: 'nri_taxation'
+//     }
+//   ],
+//   fema: [
+//     {
+//       name: 'Foreign Direct Investment',
+//       value: 'foreign_direct_investment'
+//     },
+//     {
+//       name: 'Overseas Investment',
+//       value: 'overseas_investment'
+//     },
+//     {
+//       name: 'External Commercial Borrowings',
+//       value: 'external_commercial_borrowings'
+//     },
+//     {
+//       name: 'Dealing In Immovable properties by NRI',
+//       value: 'dealing_in_immovable_properties_by_nri'
+//     }
+//   ],
+//   global_business_setup: [
+//     {
+//       name: 'United Arab Emirates(UAE)',
+//       value: 'uae'
+//     },
+//     {
+//       name: 'United States of America(USA)',
+//       value: 'usa'
+//     },
+//     {
+//       name: 'Canada',
+//       value: 'canada'
+//     }
+//   ]
+// }
 
 const pageSize = 6;
 export default function Blogs({ type }) {
--- src/pages/BlogDetail.js
+++ src/pages/BlogDetail.js
@@ -9,85 +9,90 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import PageHeader from '../components/PageHeader';
 import { Download } from 'react-ionicons'
 import { downlodFile } from '../utils';
+import { categories, subCategories } from '../constant/category';
 
 const mobileWidth = 992;
 
 
-const categories = [
-  {
-    name: 'Valuation Scoop',
-    value: 'valuation'
-  },
-  {
-    name: 'Tax Tidings',
-    value: 'tax'
-  },
-  {
-    name: 'Fema Flash',
-    value: 'fema'
-  },
-  {
-    name: 'Global Business Setup',
-    value: 'global_business_setup'
-  }
-];
+// const categories = [
+//   {
+//     name: 'Valuation Scoop',
+//     value: 'valuation'
+//   },
+//   {
+//     name: 'Tax Tidings',
+//     value: 'tax'
+//   },
+//   {
+//     name: 'Fema Flash',
+//     value: 'fema'
+//   },
+//   {
+//     name: 'Global Business Setup',
+//     value: 'global_business_setup'
+//   },
+//   {
+//     name: 'Startup Sparkle',
+//     value: 'startup'
+//   }
+// ];
 
-const subCategories = {
-  tax: [
-    {
-      name: 'Direct Tax',
-      value: 'direct_tax'
-    },
-    {
-      name: 'International Tax',
-      value: 'international_tax'
-    },
-    {
-      name: 'Transfer Pricing',
-      value: 'transfer_pricing'
-    },
-    {
-      name: 'GST',
-      value: 'gst'
-    },
-    {
-      name: 'NRI taxation',
-      value: 'nri_taxation'
-    }
-  ],
-  fema: [
-    {
-      name: 'Foreign Direct Investment',
-      value: 'foreign_direct_investment'
-    },
-    {
-      name: 'Overseas Investment',
-      value: 'overseas_investment'
-    },
-    {
-      name: 'External Commercial Borrowings',
-      value: 'external_commercial_borrowings'
-    },
-    {
-      name: 'Dealing In Immovable properties by NRI',
-      value: 'dealing_in_immovable_properties_by_nri'
-    }
-  ],
-  global_business_setup: [
-    {
-      name: 'United Arab Emirates(UAE)',
-      value: 'uae'
-    },
-    {
-      name: 'United States of America(USA)',
-      value: 'usa'
-    },
-    {
-      name: 'Canada',
-      value: 'canada'
-    }
-  ]
-}
+// const subCategories = {
+//   tax: [
+//     {
+//       name: 'Direct Tax',
+//       value: 'direct_tax'
+//     },
+//     {
+//       name: 'International Tax',
+//       value: 'international_tax'
+//     },
+//     {
+//       name: 'Transfer Pricing',
+//       value: 'transfer_pricing'
+//     },
+//     {
+//       name: 'GST',
+//       value: 'gst'
+//     },
+//     {
+//       name: 'NRI taxation',
+//       value: 'nri_taxation'
+//     }
+//   ],
+//   fema: [
+//     {
+//       name: 'Foreign Direct Investment',
+//       value: 'foreign_direct_investment'
+//     },
+//     {
+//       name: 'Overseas Investment',
+//       value: 'overseas_investment'
+//     },
+//     {
+//       name: 'External Commercial Borrowings',
+//       value: 'external_commercial_borrowings'
+//     },
+//     {
+//       name: 'Dealing In Immovable properties by NRI',
+//       value: 'dealing_in_immovable_properties_by_nri'
+//     }
+//   ],
+//   global_business_setup: [
+//     {
+//       name: 'United Arab Emirates(UAE)',
+//       value: 'uae'
+//     },
+//     {
+//       name: 'United States of America(USA)',
+//       value: 'usa'
+//     },
+//     {
+//       name: 'Canada',
+//       value: 'canada'
+//     }
+//   ]
+// }
 
 
 export default function BlogDetail(props) {
